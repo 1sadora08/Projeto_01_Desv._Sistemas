db42aba40911f455b1383ed85920ff541db592b6
Isadora <162151148+1sadora08@users.noreply.github.com>
2025-06-05 08:58:24 -0300

    Add files via upload

---
+++ Projeto_01_Desv._Sistemas-main/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config(); 
+
+const db = require("./db");
+
+const port = process.env.PORT;
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// Rota para listar todos os clientes
+app.get('/client', async (req, res) => {
+    // Chama a função que seleciona os clientes no banco de dados
+    const clientes = await db.selectCustomers();
+    // Envia a resposta em formato JSON contendo os clientes
+    res.json(clientes);
+    });
+
+app.post("/client", async (req, res) => {
+    await db.insertCustomer(req.body)
+    res.sendStatus(201)
+})
+
+// Rota para excluir cliente
+app.delete("/client/:id", async (req, res) => {
+    await db.deleteCustomer(req.params.id)
+    res.sendStatus(204)
+})
+
+app.listen(port);
+
+console.log("Backend Rodando!")
